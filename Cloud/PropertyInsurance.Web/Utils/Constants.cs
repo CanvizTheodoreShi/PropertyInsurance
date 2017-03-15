7cab7a7e0089cf35d5ce9db721c6e0c7568cce72
CanvizTheodoreShi <theodore.shi@canviz.com>
2017-03-15 14:49:15 +0800

    submit

---
+++ Cloud/PropertyInsurance.Web/Utils/Constants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PropertyInsurance.Web.Utils
+{
+    public static class Constants
+    {
+        public static readonly string AADClientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        public static readonly string AADClientSecret = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        public static readonly string AADTenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+
+        public static readonly string ClaimApproverUrl = ConfigurationManager.AppSettings["ClaimApproverUrl"];
+
+        public static readonly string AADInstance = "https://login.microsoftonline.com/";
+
+        public static string ResourceUrl = ConfigurationManager.AppSettings["ida:GraphUrl"];
+        public const string GraphResourceRootUrl = "https://graph.microsoft.com/";
+        public const string GraphResourceAADRootUrl = "https://graph.microsoft.net/";
+        public static string ConsentRedirectUrl = ConfigurationManager.AppSettings["ConsentRedirectUrl"];
+
+        public static readonly string BingMapKey = ConfigurationManager.AppSettings["BingMapKey"];
+
+        public static readonly string SourceCodeRepositoryUrl = ConfigurationManager.AppSettings["SourceCodeRepositoryUrl"];
+
+        public static readonly string O365GroupConversationsUrl = "https://outlook.office.com/owa/?path=/group/{0}/mail&exsvurl=1&ispopout=0";
+
+        public const string Base64Prefix = "data:image/jpeg;base64,";
+        public const string ThumbnailExceptionCode = "Request_ResourceNotFound";
+        public const string AnonymPhotoPath = "~/Content/images/Contoso-Insurance-Misc-Icons-PROFILE.svg";
+
+        public const int BlobReadExpireMinitues = 5;
+        public static readonly string StorageConnectionString = ConfigurationManager.AppSettings["AzureStorageAccountConnectionString"];
+        public static readonly string pictureBlobcontainer = ConfigurationManager.AppSettings["blobcontainer"];
+        public static readonly string blobImageName = ConfigurationManager.AppSettings["blobImageName"];
+
+        public const string LostTokenErrorMsg = "Authorization Required.";
+
+    }
+}
